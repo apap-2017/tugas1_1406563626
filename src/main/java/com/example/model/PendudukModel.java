4ec368cc5f3e17339ef9542aaf51eef08b1219dc
Hasya Pasaribu <hasyapasaribu@gmail.com>
2017-10-21 11:19:57 +0700

    change is_wafat and is_wni data type from int to boolean

--- src/main/java/com/example/model/PendudukModel.java
+++ src/main/java/com/example/model/PendudukModel.java
@@ -19,12 +19,28 @@ public class PendudukModel {
     private String tempat_lahir;
     private Date tanggal_lahir;
     private int jenis_kelamin;
-    private int is_wni;
+    private boolean is_wni;
     private int id_keluarga;
     private String agama;
     private String pekerjaan;
     private String status_perkawinan;
     private String status_dalam_keluarga;
     private String golongan_darah;
-    private int is_wafat;
+    private boolean is_wafat;
+
+    public boolean isIs_wni() {
+        return is_wni;
+    }
+
+    public void setIs_wni(boolean is_wni) {
+        this.is_wni = is_wni;
+    }
+
+    public boolean isIs_wafat() {
+        return is_wafat;
+    }
+
+    public void setIs_wafat(boolean is_wafat) {
+        this.is_wafat = is_wafat;
+    }
 }
